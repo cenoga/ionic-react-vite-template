2e7499ef65d3fce4d527d1d393d142784e536928
cenoga38 <cenoga38@gmail.com>
2023-06-06 13:16:52 +0800

    Add existing project files to Git

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+const queryClient = new QueryClient();
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>
+);
